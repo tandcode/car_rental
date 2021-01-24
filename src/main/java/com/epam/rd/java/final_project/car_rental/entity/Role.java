57ca8a0cb9be443fb55d74cecc0b48436f3243a4
tandcode <tandcode@gmail.com>
2021-01-24 16:45:19 +0200

    Main page cars added

--- src/main/java/com/epam/rd/java/final_project/car_rental/entity/Role.java
+++ src/main/java/com/epam/rd/java/final_project/car_rental/entity/Role.java
@@ -2,14 +2,8 @@ package com.epam.rd.java.final_project.car_rental.entity;
 
 import org.springframework.security.core.GrantedAuthority;
 
-import javax.persistence.ManyToMany;
-import java.util.Set;
-
 public enum  Role implements GrantedAuthority{
     USER, ADMIN, MANAGER;
-
-//    @ManyToMany
-//    private Set<User> users;
 
     @Override
     public String getAuthority() {
