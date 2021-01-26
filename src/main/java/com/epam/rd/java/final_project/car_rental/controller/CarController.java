9b9e2c4eebdca5386d171eb47804de7d91de4d90
tandcode <tandcode@gmail.com>
2021-01-26 22:26:28 +0200

    Working sorting

--- src/main/java/com/epam/rd/java/final_project/car_rental/controller/CarController.java
+++ src/main/java/com/epam/rd/java/final_project/car_rental/controller/CarController.java
@@ -6,7 +6,6 @@ import com.epam.rd.java.final_project.car_rental.service.CarService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -29,14 +28,25 @@ public class CarController {
     @GetMapping
     private String carList(Model model,
                            @RequestParam("page") Optional<Integer> page,
-                           @RequestParam("size") Optional<Integer> size
+                           @RequestParam("size") Optional<Integer> size,
+                           @RequestParam("sortField") Optional<String> sortingField,
+                           @RequestParam("sortDir") Optional<String> sortingDir
     ){
         int currentPage = page.orElse(1);
         int pageSize = size.orElse(5);
+        String sortField = sortingField.orElse("carBrand");
+        String sortDir = sortingDir.orElse("asc");
 
-        Page<Car> carPage = carService.findPaginated(PageRequest.of(currentPage - 1, pageSize));
+        Page<Car> carPage = carService.findPaginated(
+                currentPage,
+                pageSize,
+                sortField,
+                sortDir);
 
         model.addAttribute("carPage", carPage);
+        model.addAttribute("sortField", sortField);
+        model.addAttribute("sortDir", sortDir);
+        model.addAttribute("reverseSortDir", sortDir.equals("asc") ? "desc" : "asc");
 
         int totalPages = carPage.getTotalPages();
         if (totalPages > 0) {
--- src/main/java/com/epam/rd/java/final_project/car_rental/service/CarService.java
+++ src/main/java/com/epam/rd/java/final_project/car_rental/service/CarService.java
@@ -3,11 +3,11 @@ package com.epam.rd.java.final_project.car_rental.service;
 import com.epam.rd.java.final_project.car_rental.entity.Car;
 import com.epam.rd.java.final_project.car_rental.repository.CarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.*;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
-
-import java.util.Collections;
-import java.util.List;
 
 @Service
 public class CarService {
@@ -15,11 +15,13 @@ public class CarService {
     @Autowired
     CarRepository carRepository;
 
-    public Page<Car> findPaginated(Pageable pageable) {
+    public Page<Car> findPaginated(int currentPage, int pageSize, String sortField, String sortDirection) {
+        Sort sort = sortDirection.equalsIgnoreCase(Sort.Direction.ASC.name())
+                ? Sort.by(sortField).ascending()
+                : Sort.by(sortField).descending();
+
+        Pageable pageable = PageRequest.of(currentPage - 1, pageSize, sort);
         return carRepository.findAll(pageable);
     }
 
-//    public Iterable<Car> findSorted(Sort sort) {
-//
-//    }
 }
