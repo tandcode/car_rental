51d3fbc97f88e740b8303a1a7299f02840777617
tandcode <tandcode@gmail.com>
2021-01-26 20:50:36 +0200

    Working pagination with cars per page

--- src/main/java/com/epam/rd/java/final_project/car_rental/service/CarService.java
+++ src/main/java/com/epam/rd/java/final_project/car_rental/service/CarService.java
@@ -3,10 +3,7 @@ package com.epam.rd.java.final_project.car_rental.service;
 import com.epam.rd.java.final_project.car_rental.entity.Car;
 import com.epam.rd.java.final_project.car_rental.repository.CarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.*;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
@@ -19,21 +16,10 @@ public class CarService {
     CarRepository carRepository;
 
     public Page<Car> findPaginated(Pageable pageable) {
-        int pageSize = pageable.getPageSize();
-        int currentPage = pageable.getPageNumber();
-        int startItem = currentPage * pageSize;
-        List<Car> list;
-
-        if (carRepository.findAll().size() < startItem) {
-            list = Collections.emptyList();
-        } else {
-            int toIndex = Math.min(startItem + pageSize, carRepository.findAll().size());
-            list = carRepository.findAll().subList(startItem, toIndex);
-        }
-
-        Page<Car> bookPage
-                = new PageImpl<Car>(list, PageRequest.of(currentPage, pageSize), carRepository.findAll().size());
-
-        return bookPage;
+        return carRepository.findAll(pageable);
     }
+
+//    public Iterable<Car> findSorted(Sort sort) {
+//
+//    }
 }
